91a5127be01ab713a473f5655950ad42e3b3b628
romulo jensen <romul@RomuloJensen.(none)>
2022-10-28 20:59:52 -0300

    Validadores de senha e login

--- lib/auth/model/usuario.dart
+++ lib/auth/model/usuario.dart
@@ -5,4 +5,25 @@ class Usuario {
     required this.login,
     required this.senha,
   });
+
+  String? validateSenha(String? senha) {
+    if (senha == null) return 'Digitar uma senha';
+    if (senha.length < 8) {
+      return 'Senha deve conter mais de 8 caracteres';
+    }
+    if (!senha.contains(RegExp(r'[A-Z]+'))) {
+      return 'Senha deve conter letras maiusculas';
+    }
+    if (!senha.contains(RegExp(r'[a-z]+'))) {
+      return 'Senha deve conter letras minusculas';
+    }
+    if (!senha.contains(RegExp(r'\d'))) {
+      return 'Senha deve conter numeros';
+    }
+    return null;
+  }
+
+  String? validateLogin(String? login) {
+    return null;
+  }
 }
