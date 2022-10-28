9418c90f57a385451855ce2848369ce4c5bafea4
romulo jensen <romul@RomuloJensen.(none)>
2022-10-28 20:59:28 -0300

    Providers para Usuario e Carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_food/auth/model/usuario.dart';
 import 'package:shopping_food/carrinho/presentation/detalhes.dart';
 import 'package:shopping_food/restaurante/application/listagem_store.dart';
 import 'package:shopping_food/restaurante/model.dart';
@@ -6,6 +8,7 @@ import 'package:shopping_food/restaurante/presentation/detalhes.dart';
 import 'package:shopping_food/restaurante/presentation/listagem.dart';
 
 import 'auth/presentation/login.dart';
+import 'carrinho/domain/carrinho.dart';
 import 'produto/presentation/detalhes.dart';
 import 'restaurante/application/detalhes_store.dart';
 
@@ -19,44 +22,50 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => Usuario(login: '', senha: '')),
+        Provider(create: (_) => Carrinho(itens: [])),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          ),
         ),
-      ),
-      initialRoute: Login.route,
-      onGenerateRoute: (settings) {
-        final args = settings.arguments as Map<String, dynamic>?;
-        final routes = <String, WidgetBuilder>{
-          Login.route: (_) => const Login(),
-          RestauranteListagem.route: (_) =>
-              RestauranteListagem(store: ListagemRestaurantesStore()),
-          RestauranteDetalhes.route: (_) => RestauranteDetalhes(
-              restaurante: args!['restaurante'],
-              store: RestauranteDetalhesStore()),
-          ProdutoDetalhes.route: (_) => ProdutoDetalhes(
-                produto: args!['produto'],
-                restaurante: args['restaurante'],
+        initialRoute: Login.route,
+        onGenerateRoute: (settings) {
+          final args = settings.arguments as Map<String, dynamic>?;
+          final routes = <String, WidgetBuilder>{
+            Login.route: (_) => const Login(),
+            RestauranteListagem.route: (_) =>
+                RestauranteListagem(store: ListagemRestaurantesStore()),
+            RestauranteDetalhes.route: (_) => RestauranteDetalhes(
+                restaurante: args!['restaurante'],
+                store: RestauranteDetalhesStore()),
+            ProdutoDetalhes.route: (_) => ProdutoDetalhes(
+                  produto: args!['produto'],
+                  restaurante: args['restaurante'],
+                ),
+            CarrinhoDetalhes.route: (_) => const CarrinhoDetalhes(),
+          };
+          final builder = routes[settings.name];
+          if (builder == null) {
+            return MaterialPageRoute(
+              builder: (context) => const Scaffold(
+                body: Center(child: Text('Nao Encontrado')),
               ),
-          CarrinhoDetalhes.route: (_) => const CarrinhoDetalhes(),
-        };
-        final builder = routes[settings.name];
-        if (builder == null) {
-          return MaterialPageRoute(
-            builder: (context) => const Scaffold(
-              body: Center(child: Text('Nao Encontrado')),
-            ),
-          );
-        } else {
-          return MaterialPageRoute(builder: builder);
-        }
-      },
+            );
+          } else {
+            return MaterialPageRoute(builder: builder);
+          }
+        },
+      ),
     );
   }
 }
