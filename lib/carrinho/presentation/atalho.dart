bae07ff3635e198ee7f6ae0e6527ea18cadf7abc
romulo jensen <romul@RomuloJensen.(none)>
2022-10-28 21:01:20 -0300

    Provider package e icone de carrinho

--- lib/carrinho/presentation/atalho.dart
+++ lib/carrinho/presentation/atalho.dart
@@ -7,7 +7,7 @@ class CarrinhoIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: Icon(Icons.gif_box),
+      icon: const Icon(Icons.shopping_cart),
       onPressed: () => Navigator.of(context).pushNamed(CarrinhoDetalhes.route),
     );
   }
